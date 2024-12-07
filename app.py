f0c176caa49fe4a90fe70ca976fa59be905b8376
Fernando Nakamuta <fernandonakamuta@gmail.com>
2024-12-07 01:51:11 -0300

    Remove guardrails for deploy

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
 from openai import OpenAI
 import streamlit as st
-from guardrails.hub import RestrictToTopic
-from guardrails import Guard
 
 st.title("Hakura Orientações Pré-Hospistalares")
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-# Setup Guard with RestrictToTopic validator
-guard = Guard().use(
-    RestrictToTopic(
-        valid_topics=["greeting", "health", "accident", "patient", "doctor"],
-        disable_classifier=True,
-        disable_llm=False,
-        on_fail="exception"  # Raise exception on invalid input
-    )
-)
 
 # Initialize the OpenAI model
 if "openai_model" not in st.session_state:
@@ -35,48 +23,48 @@ if "messages" not in st.session_state:
                 2. Caso o paciente faça perguntas sobre osteogênese imperfeita, responda com informações claras, baseadas em evidências científicas, e adaptadas à linguagem leiga. 
                 3. Não dar diagnósticos nem condutas ou recomendações de remédios , no caso de sintomas e dores deve recomendar que o paciente vá ao médico!
                 4. Se o paciente relatar ter sofrido um acidente: 
-                - Pergunte como foi o acidente.
-                - Classifique o acidente com base na área afetada, escolhendo entre as seguintes opções:
-                    - *braço*
-                    - *antebraço*
-                    - *cabeça*
-                    - *joelho*
-                    - *perna*
-                    - *coxa*
-                    - *tórax*
-                - Salve a área identificada no parâmetro <local_acidente>.
-                
+                   - Pergunte como foi o acidente.
+                   - Classifique o acidente com base na área afetada, escolhendo entre as seguintes opções:
+                     - *braço*
+                     - *antebraço*
+                     - *cabeça*
+                     - *joelho*
+                     - *perna*
+                     - *coxa*
+                     - *tórax*
+                   - Salve a área identificada no parâmetro <local_acidente>.
+
                 ### Recomendações baseadas no <local_acidente>:
                 1. *Braço*:  
-                - Assista ao vídeo de orientação: [Imobilização do Braço](https://www.youtube.com/watch?v=yiB04G30Eww).  
-                - Realize a imobilização conforme descrito no vídeo.  
-                - Procure um pronto-socorro para radiografias.
+                   - Assista ao vídeo de orientação: [Imobilização do Braço](https://www.youtube.com/watch?v=yiB04G30Eww).  
+                   - Realize a imobilização conforme descrito no vídeo.  
+                   - Procure um pronto-socorro para radiografias.
 
                 2. *Antebraço*:  
-                - Assista ao vídeo de orientação: [Imobilização do Antebraço](https://www.youtube.com/watch?v=dFmDD0B4kz0).  
-                - Realize a imobilização conforme descrito no vídeo.  
-                - Procure um pronto-socorro para radiografias.
+                   - Assista ao vídeo de orientação: [Imobilização do Antebraço](https://www.youtube.com/watch?v=dFmDD0B4kz0).  
+                   - Realize a imobilização conforme descrito no vídeo.  
+                   - Procure um pronto-socorro para radiografias.
 
                 3. *Cabeça*:  
-                - Procure um pronto-socorro urgentemente para avaliação de possível trauma cranioencefálico.
+                   - Procure um pronto-socorro urgentemente para avaliação de possível trauma cranioencefálico.
 
                 4. *Joelho*:  
-                - Assista ao vídeo de orientação: [Imobilização do Joelho](https://www.youtube.com/watch?v=6xs2SdilvME).  
-                - Realize a imobilização conforme descrito no vídeo.  
-                - Procure um pronto-socorro para radiografias.
+                   - Assista ao vídeo de orientação: [Imobilização do Joelho](https://www.youtube.com/watch?v=6xs2SdilvME).  
+                   - Realize a imobilização conforme descrito no vídeo.  
+                   - Procure um pronto-socorro para radiografias.
 
                 5. *Perna*:  
-                - Assista ao vídeo de orientação: [Imobilização da Perna](https://www.youtube.com/watch?v=zSCxE3-Q0FI).  
-                - Realize a imobilização conforme descrito no vídeo.  
-                - Procure um pronto-socorro para radiografias.
+                   - Assista ao vídeo de orientação: [Imobilização da Perna](https://www.youtube.com/watch?v=zSCxE3-Q0FI).  
+                   - Realize a imobilização conforme descrito no vídeo.  
+                   - Procure um pronto-socorro para radiografias.
 
                 6. *Coxa*:  
-                - Assista ao vídeo de orientação: [Imobilização da Coxa](https://www.youtube.com/watch?v=gRMBk_h-x9M).  
-                - Realize a imobilização conforme descrito no vídeo.  
-                - Procure um pronto-socorro para radiografias.
+                   - Assista ao vídeo de orientação: [Imobilização da Coxa](https://www.youtube.com/watch?v=gRMBk_h-x9M).  
+                   - Realize a imobilização conforme descrito no vídeo.  
+                   - Procure um pronto-socorro para radiografias.
 
                 7. *Tórax*:  
-                - Procure um pronto-socorro urgentemente para avaliação de possível trauma toráxico. 
+                   - Procure um pronto-socorro urgentemente para avaliação de possível trauma toráxico. 
 
                 ### Observações:
                 - Sempre ofereça recomendações baseadas na gravidade do caso e priorize a busca por atendimento médico adequado.
@@ -106,35 +94,28 @@ for message in st.session_state.messages:
 # User input
 prompt = st.chat_input("Digite sua mensagem...")
 if prompt:
-    # Validate user input with Guard
-    try:
-        guard.validate(prompt)  # Raise an exception if validation fails
+    # Display user message in the chat container
+    with st.chat_message(name="user"):
+        st.write(prompt)
 
-        # Display user message in the chat container
-        with st.chat_message(name="user"):
-            st.write(prompt)
+    # Add user message to the chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-        # Add user message to the chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        try:
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        except Exception as e:
+            response = f"Error: {e}"
+            st.write(response)
 
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            try:
-                stream = client.chat.completions.create(
-                    model=st.session_state["openai_model"],
-                    messages=[
-                        {"role": m["role"], "content": m["content"]}
-                        for m in st.session_state.messages
-                    ],
-                    stream=True,
-                )
-                response = st.write_stream(stream)
-            except Exception as e:
-                response = f"Error: {e}"
-                st.write(response)
-
-        # Add assistant's response to the history
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
-    except Exception:
-        st.error("Por favor, insira uma mensagem relacionada a tópicos de saúde ou acidentes.")
+    # Add assistant's response to the history
+    st.session_state.messages.append({"role": "assistant", "content": response})
